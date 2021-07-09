aa0b293336d77965989c6ff379349dade8b173e4
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-07-09 17:48:10 +0000

    fixed team_mailer

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -51,7 +51,7 @@ class TeamsController < ApplicationController
     @team = Team.friendly.find(params[:format])
     @new_owner = User.find(params[:id])
     @team.update_attributes(owner_id: @new_owner.id)
-      TeamMailer.change_owner_mail(@new_owner,@team).deliver
+      TeamMailer.team_mail(@new_owner,@team).deliver
       redirect_to @team, notice: "オーナーを変更しました"
   end
 
--- app/mailers/team_mailer.rb
+++ app/mailers/team_mailer.rb
@@ -1,9 +1,9 @@
 class TeamMailer < ApplicationMailer
   default from: 'from@example.com'
 
-  def change_owner_mail(new_owner,team)
+  def team_mail(new_owner,team)
     @new_owner = new_owner
     @team = team
-    mail to: @new_owner.email, @team.email, subject: `オーナー権限が移動しました`
+    mail to: @new_owner.email, subject: `オーナー権限が移動しました`
   end
 end
