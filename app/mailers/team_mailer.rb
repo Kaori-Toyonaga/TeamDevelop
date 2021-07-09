63be87ead3eb3862ed4fd13c71cc47cc079a0e1a
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-07-09 13:16:55 +0000

    fixed team mailer

--- app/mailers/team_mailer.rb
+++ app/mailers/team_mailer.rb
@@ -1,8 +1,9 @@
 class TeamMailer < ApplicationMailer
   default from: 'from@example.com'
 
-  def change_owner_mail(new_owner)
+  def change_owner_mail(new_owner,team)
     @new_owner = new_owner
-    mail to: @new_owner.email, subject: `オーナー権限が移動しました`
+    @team = team
+    mail to: @new_owner.email, @team.email, subject: `オーナー権限が移動しました`
   end
 end
